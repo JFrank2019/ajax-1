b3d8d3d483a864498020b3e13ebfbd66ebbdca61
JFrank2019 <jfrank2019@163.com>
2020-09-01 10:05:04 +0800

    封转公用逻辑 增加数据分页校验

--- public/main.js
+++ public/main.js
@@ -1,117 +1,84 @@
 console.log('我是 main.js')
 
-getCSS.onclick = () => {
+// 简单封转ajax
+let ajax = (method, path, fn) => {
   const request = new XMLHttpRequest()
-  request.open('GET', '/style.css')
+  request.open(method, path)
   request.onreadystatechange = () => {
-    console.log(request.readyState)
-    // 下载完成，单不知道是成功 2xx 还是失败 4xx 5xx
     if (request.readyState === 4) {
-      console.log('下载完成')
       if (request.status >= 200 && request.status < 300) {
-        // 创建 style 标签
-        const style = document.createElement('style')
-        // 填写 style 内容
-        style.innerHTML = request.response
-        // 插到头里面
-        document.head.appendChild(style)
+        fn(request)
       } else {
-        alert('加载 CSS 失败')
+        alert(`请求${path}失败`)
       }
     }
   }
   request.send()
+}
+
+getCSS.onclick = () => {
+  ajax('GET', '/style.css', request => {
+    // 创建 style 标签
+    const style = document.createElement('style')
+    // 填写 style 内容
+    style.innerHTML = request.response
+    // 插到头里面
+    document.head.appendChild(style)
+  })
 }
 
 getJS.onclick = () => {
-  const request = new XMLHttpRequest()
-  request.open('GET', '/2.js')
-  request.onreadystatechange = () => {
-    if (request.readyState === 4) {
-      if (request.status >= 200 && request.status < 300) {
-        // 创建 script 标签
-        const script = document.createElement('script')
-        // 设置内容
-        script.innerHTML = request.response
-        // 添加到 body 中
-        document.body.appendChild(script)
-      } else {
-        alert('加载 JS 失败')
-      }
-    }
-  }
-  request.send()
+  ajax('GET', '/2.js', request => {
+    // 创建 script 标签
+    const script = document.createElement('script')
+    // 设置内容
+    script.innerHTML = request.response
+    // 添加到 body 中
+    document.body.appendChild(script)
+  })
 }
 
 getHTML.onclick = () => {
-  const request = new XMLHttpRequest()
-  request.open('GET', '/3.html')
-  request.onreadystatechange = () => {
-    if (request.readyState === 4) {
-      if (request.status >= 200 && request.status < 300) {
-        htmlContent.innerHTML = request.response
-      } else {
-        alert('加载 HTML 失败')
-      }
-    }
-  }
-  request.send()
+  ajax('GET', '/3.html', request => {
+    htmlContent.innerHTML = request.response
+  })
 }
 
 getXML.onclick = () => {
-  const request = new XMLHttpRequest()
-  request.open('GET', '/4.xml')
-  request.onreadystatechange = () => {
-    if (request.readyState === 4) {
-      if (request.status >= 200 && request.status < 300) {
-        let dom = request.responseXML
-        let content = dom.getElementsByTagName('warning')[0].innerHTML
-        console.log(content.trim())
-      } else {
-        alert('加载 XML 失败')
-      }
-    }
-  }
-  request.send()
+  ajax('GET', '/4.xml', request => {
+    let dom = request.responseXML
+    let content = dom.getElementsByTagName('warning')[0].innerHTML
+    console.log(content.trim())
+  })
 }
 
 getJSON.onclick = () => {
-  const request = new XMLHttpRequest()
-  request.open('GET', '/5.json')
-  request.onreadystatechange = () => {
-    if (request.readyState === 4) {
-      if (request.status >= 200 && request.status < 300) {
-        let object
-        try {
-          object = JSON.parse(request.response)
-        } catch (error) {
-          console.log('JSON转化出错了', error)
-          object = {}
-        }
-        console.log(object)
-      }
+  ajax('GET', '/5.json', request => {
+    let object
+    try {
+      object = JSON.parse(request.response)
+    } catch (error) {
+      console.log('JSON转化出错了', error)
+      object = {}
     }
-  }
-  request.send()
+    console.log(object)
+  })
 }
 
 let pageNum = 1
 
 getPage.onclick = () => {
-  const request = new XMLHttpRequest()
-  request.open('GET', `/page${pageNum}`)
-  request.onreadystatechange = () => {
-    if (request.readyState === 4) {
-      if (request.status >= 200 && request.status < 300) {
-        let array = JSON.parse(request.response)
-        array.forEach(item => {
-          const li = document.createElement('li')
-          li.textContent = item.id
-          xxx.appendChild(li)
-        })
-        pageNum += 1
-      }
-    }
+  if (pageNum > 3) {
+    alert('暂无新数据')
+    return
   }
-  request.send()
+  ajax('GET', `/page${pageNum}`, request => {
+    let array = JSON.parse(request.response)
+    array.forEach(item => {
+      const li = document.createElement('li')
+      li.textContent = item.id
+      xxx.appendChild(li)
+    })
+    pageNum += 1
+  })
 }
